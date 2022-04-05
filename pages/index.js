8d4d4d686982cac8f693f52adcb7f207330aae37
Tuaha amir <tuaha.aamir@gmail.com>
2022-04-05 15:01:06 +0500

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,61 @@
-import Head from 'next/head'
-import Layout, { siteTitle } from '../components/layout'
-import utilStyles from '../styles/utils.module.css'
+import Head from "next/head";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
+import { getSortedPostsData } from "../lib/posts";
+import Link from "next/link";
+import Date from "../components/date";
 
-export default function Home() {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Home({ allPostsData }) {
+  let data = [allPostsData];
+  let dataArr = Array.from(data);
+
+  console.warn("Data Fetched", dataArr);
+  console.warn("Data Fetcherr", Array.from([allPostsData]));
+
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
         <p>
-          (This is a sample website - you’ll be building a site like this in{' '}
+          Elon Reeve Musk FRS is an entrepreneur and business magnate. He is the
+          founder, CEO, and Chief Engineer at SpaceX; early-stage investor, CEO,
+          and Product Architect of Tesla, Inc.; founder of The Boring
+        </p>
+
+        <p>
+          (This is a sample website - you’ll be building a site like this in{" "}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
       </section>
+
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, title, date }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
+              <br />
+              {id}
+              <br />
+              <Date dateString={date} />
+            </li>
+          ))}
+        </ul>
+      </section>
     </Layout>
-  )
+  );
 }
